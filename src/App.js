4d39d13093d7acc9f0a6dfa3d3a0288a1ab2ab9e
Trent Davis <trendavisinc@gmail.com>
2020-08-18 00:36:18 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+const App = () => {
+
+    return (
+        <div>
+            <h1>React Redux</h1>
+        </div>
+    )
+}
+
+export default hot(module)(App);
